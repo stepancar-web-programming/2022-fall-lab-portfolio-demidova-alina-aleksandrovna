99d3aeab8d7e6f1be1962ea508400724b669dbb4
Milksisis <106484677+Milksisis@users.noreply.github.com>
2022-11-08 21:02:16 +0300

    Add files via upload

--- web-proect/README.md
+++ web-proect/README.md
@@ -1,1 +1,3 @@
+2022-fall-lab-portfolio-demidova-alina-aleksandrovna
 
+https://pitomets-kot-timon.netlify.app/
---
+++ web-proect/app.js
@@ -0,0 +1,29 @@
+
+/** для слайдера*/
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let slides = document.getElementsByClassName("mySlides");
+  if (n > slides.length) { slideIndex = 1; }
+  if (n < 1) { slideIndex = slides.length; }
+  for (let slide of slides) {
+    slide.style.display = "none";}
+  slides[slideIndex - 1].style.display = "flex";    
+}
+/** таймер авто переключения на 6 сек*/
+var timer = 0;
+makeTimer(); 
+function makeTimer(){
+  clearInterval(timer) 
+  timer = setInterval(function(){ slideIndex++; showSlides(slideIndex);} ,6000);
+}
+slides[slideIndex-1].style.display = "block";
+
