e0881236ae07bb1fc199b7f15e7440d2a4dc4fbc
Milksisis <106484677+Milksisis@users.noreply.github.com>
2022-11-08 20:28:00 +0300

    Create fon.js

---
+++ web-proect/fon.js
@@ -0,0 +1,38 @@
+const button = document.getElementById("button");
+const number = [
+  "0",
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+];
+
+button.addEventListener("click", () => {
+  let numberColor = generateNumber();
+  document.body.style.backgroundColor = numberColor;
+  color.textContent = numberColor;
+});
+
+function generateNumber() {
+  let numberColor = "#";
+  for (let i = 0; i < 6; i++) {
+    numberColor += number[getRandomNumber()];
+}
+
+  return numberColor;
+}
+
+function getRandomNumber() {
+    return Math.floor(Math.random() * number.length);
+}
