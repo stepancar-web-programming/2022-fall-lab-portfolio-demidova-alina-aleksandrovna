388d198e706fbc90ecd20f218e3a8a54067bb9f8
Milksisis <106484677+Milksisis@users.noreply.github.com>
2022-11-11 01:12:57 +0300

    Update and rename app.js to js/app.js

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 let slideIndex = 0
-function showSlides() {
-  const slides = document.getElementsByClassName('mySlides')
+function showslides() {
+  const slides = document.getElementsByClassName('myslides')
   for (let i = 0; i < slides.length; i += 1) {
     slides[i].style.display = 'none'
   }
@@ -9,6 +9,6 @@ function showSlides() {
     slideIndex = 1
   }
   slides[slideIndex - 1].style.display = 'block'
-  setTimeout(showSlides, 3000)
+  setTimeout(showslides, 3000)
 }
-showSlides()
+showslides()
