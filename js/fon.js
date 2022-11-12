a32fc27c4383bff6b65498269af03d5ca4a74251
Milksisis <106484677+Milksisis@users.noreply.github.com>
2022-11-12 15:24:45 +0300

    Update fon.js

--- js/fon.js
+++ js/fon.js
@@ -1,37 +1,7 @@
 const button = document.getElementById('button')
-const number = [
-  '0',
-  '1',
-  '2',
-  '3',
-  '4',
-  '5',
-  '6',
-  '7',
-  '8',
-  '9',
-  'A',
-  'B',
-  'C',
-  'D',
-  'E',
-  'F',
-]
-
-function getRandomNumber() {
-  return Math.floor(Math.random() * number.length)
-}
-
-function generateNumber() {
-  let numberColor = '#'
-
-  for (let i = 0; i < 6; i += 1) {
-    numberColor += number[getRandomNumber()]
-  }
-  return numberColor
-}
 
 button.addEventListener('click', () => {
-  const numberColor = generateNumber()
-  document.body.style.backgroundColor = numberColor
+  let Color = '#'
+  Color += Math.random().toString(16).slice(2, 8)
+  document.body.style.backgroundColor = Color
 })
